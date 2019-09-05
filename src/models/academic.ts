90548902fd719577e3281659d846419fcc612014
Artemchuk Dmytro <taruuuch@gmail.com>
2019-09-06 00:16:51 +0300

    Add to models base structure for create model in db

--- src/models/academic.ts
+++ src/models/academic.ts
@@ -1,18 +1,9 @@
 import mongoose from "mongoose";
-
 let Schema = mongoose.Schema;
 
 let AcademicSchema = new Schema({
-	groupId: String,
+	academicId: String,
 	title: String,
-	specialityId: Number,
-	teacherId: Number,
-	educationLevelId: Number,
-	educationFormId: Number,
-	studentCount: Number,
-	isActive: Boolean,
 });
 
-let Academic = mongoose.model("Academic", AcademicSchema);
-
-export default Academic;
+export default mongoose.model('Academic', AcademicSchema);
