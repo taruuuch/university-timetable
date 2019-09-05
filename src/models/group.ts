c84d3a8bb16bf7176d73f7fc71f6f9b514ff68d1
Artemchuk Dmytro <taruuuch@gmail.com>
2019-09-05 18:29:22 +0300

    Add test group model

--- src/models/group.ts
+++ src/models/group.ts
@@ -1,2 +1,18 @@
-import maraquia from "maraquia";
+import mongoose from "mongoose";
 
+let Schema = mongoose.Schema;
+
+let GroupSchema = new Schema({
+	groupId: String,
+	title: String,
+	specialityId: Number,
+	teacherId: Number,
+	educationLevelId: Number,
+	educationFormId: Number,
+	studentCount: Number,
+	isActive: Boolean,
+});
+
+let Group = mongoose.model("Group", GroupSchema);
+
+export default Group;
