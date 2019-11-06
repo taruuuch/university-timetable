3ec73c87d7c411d58dd46a8d8ca8d7ec6f2de66b
Artemchuk Dmytro <taruuuch@gmail.com>
2019-11-06 18:37:45 +0200

    Add json data for init database

--- models/specialityModel.js
+++ models/specialityModel.js
@@ -5,6 +5,7 @@ let Schema = mongoose.Schema;
 const SpecialitySchema = new Schema({
 	_id: Schema.Types.ObjectId,
 	title: String,
+	code: Number,
 	facultyId: [{
 		type: Schema.Types.ObjectId, ref: 'Faculty'
 	}]
